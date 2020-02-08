9c3e4d24e6239611ce5bffe0fbf4358d8b959fde
hi_melnikov <melnikovam10@gmail.com>
2020-02-08 14:07:33 +0300

    first part of front is ready

--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-#костыльную оптимизацию не убирать
-data = {}
 
 @app.route('/', methods = ["GET"])
-def get():
+def index_get():
     return render_template("index.html")
 
-@app.route('/', methods = ["POST"])
-def post():
-    if "user_id" in request.form and "start_date" in request.form and "end_date" in request.form:
-        user_id = request.form["user_id"]
-        start_date = request.form["start_date"]
-        end_date = request.form["end_date"]
+@app.route('/results', methods = ["GET"])
+def results_get():
+    return redirect("/")
+
+@app.route('/results', methods = ["POST"])
+def results_post():
+
+    user_id = request.form["user_id"]
+    start_date = request.form["start_date"]
+    end_date = request.form["end_date"]
+    if user_id and start_date and end_date:
         print(user_id, start_date, end_date)
-    return render_template("index.html")
+
+        return render_template("results.html", start_date = start_date, end_date = end_date, results )
+    return redirect("/")
 
 
 
